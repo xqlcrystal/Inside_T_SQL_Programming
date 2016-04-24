184ac6e48343204c0c4b1677502741a0e433ba17
xieql2 <xieql2@lenovo.com>
2016-04-24 14:00:13 +0800

    5-4sql2005新特性

---
+++ Inside T SQL Programming/5/5-4/VSalesRN.sql
@@ -0,0 +1,12 @@
+﻿if OBJECT_ID('dbo.VSalesRN') IS NOT NULL
+	drop view dbo.VSalesRN;
+Go
+
+Create View dbo.VSalesRN
+As
+
+Select mnth,qty,ROW_NUMBER() over(Order by mnth) as rn
+from dbo.Sales;
+go
+
+
