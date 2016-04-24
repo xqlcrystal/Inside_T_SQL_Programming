e39b2f0bd4fb818b113a428e872a377fa8c31f68
xieql2 <xieql2@lenovo.com>
2016-04-24 14:15:22 +0800

    vtrends

---
+++ Inside T SQL Programming/5/5-4/Vtrends.sql
@@ -0,0 +1,17 @@
+﻿if OBJECT_ID('dbo.Vtrends') IS NOT NULL
+	drop view dbo.Vtrends;
+go
+
+create view dbo.Vtrends
+as 
+
+select CONVERT(varchar(6),MIN(mnth),112) as start_range,
+	CONVERT(varchar(6),max(mnth),112) as end_range,
+	case sgn
+		when -1 then 'down'
+		when 0 then 'same'
+		when 1 then 'up'
+		else 'unknown'
+	end as trend　
+from dbo.Vgrp
+group by sgn,grp
