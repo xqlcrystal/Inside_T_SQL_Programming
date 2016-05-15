8b1f0b877a4efc3bb4e331b2f036ae439da324a7
xqlcrystal <xql252660@163.com>
2016-05-15 21:30:12 +0800

    视图索引

---
+++ Inside T SQL Programming/5/5-7/vempOrders.sql
@@ -0,0 +1,17 @@
+﻿if OBJECT_ID('dbo.vempOrders') is not null
+	drop view dbo.vempOrders;
+go
+
+create view dbo.vempOrders with schemabinding
+as
+
+select O.employeeID,SUM(OD.Quantity) as TotalQty,COUNT_BIG(*) as cnt
+from dbo.Orders as O
+	join dbo.[Order details] as OD
+on O.Orderid=OD.Orderid
+group by O.employeeID;
+
+go
+
+create unique clustered index idx_uc_empid on dbo.vempOrders(EmployeeID);
+go
