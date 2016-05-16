c2f9bb0c8960f118c7b5eb9e80bf0e5be2dc9a9b
xqlcrystal <xql252660@163.com>
2016-05-16 17:54:41 +0800

    UDF

---
+++ Inside T SQL Programming/6/6-2/fn_GetcustOrders.sql
@@ -0,0 +1,29 @@
+﻿set nocount on ;
+use NorthWind;
+go
+
+if OBJECT_ID('dbo.fn_GetcustOrders') is not null
+	drop function dbo.fn_GetcustOrders;
+go
+
+create function dbo.fn_GetcustOrders
+(@cid as nchar(5)) returns table
+as
+return 
+	select OrderID,CustomerID,EmployeeID,OrderDate,RequiredDate,ShippedDate,ShipVia,
+	Freight,ShipName,ShipAddress,ShipCity,ShipRegion,ShipPostalCode,ShipCountry
+	from dbo.Orders
+	where CustomerID=@cid;
+go
+
+
+select o.OrderID,o.CustomerID,OD.ProductID,OD.Quantity
+from dbo.fn_GetcustOrders(N'ALFKI') as O
+join dbo.[Order Details] as OD
+on o.OrderID=OD.OrderID
+
+begin tran
+select OrderID,ShipVia from dbo.fn_GetcustOrders(N'ALFKI') As O;
+update dbo.fn_GetcustOrders(N'ALFKI') set ShipVia=2;
+select OrderID,ShipVia from dbo.fn_GetcustOrders(N'ALFKI') As O;
+rollback
---
+++ Inside T SQL Programming/6/6-2/fn_add1.sql
@@ -0,0 +1,57 @@
+﻿if OBJECT_ID('dbo.T1') is not null
+	drop table dbo.T1;
+go
+
+create table dbo.T1
+(
+keycol int not null constraint pk_T1 primary key check(keycol>0),
+datacol varchar(10) not null
+);
+
+go
+
+if OBJECT_ID('dbo.fn_T1_getkey') is not null
+	drop function dbo.fn_T1_getkey;
+go
+
+create function dbo.fn_T1_getkey() returns int
+as
+begin
+	return
+	case
+		when not exists(select * from dbo.T1 where keycol=1) then 1
+	else
+		(select min(keycol+1)
+		from dbo.T1 as A
+		where not exists(select * from dbo.T1 as B where B.keycol=A.keycol+1)
+		)
+	end;
+
+end
+go
+
+alter table dbo.T1 add default(dbo.fn_T1_getkey()) for keycol;
+
+insert into dbo.T1(datacol) values('a');
+insert into dbo.T1(datacol) values('b');
+insert into dbo.T1(datacol) values('c');
+
+select * from dbo.T1;
+
+alter function dbo.fn_add1(@i as int) returns int
+	with schemabinding
+as 
+begin
+	return @i+1;
+
+end
+
+alter table dbo.T1 add col1 as dbo.fn_add1(keycol) constraint uq_T1_col1 unique;
+
+select * from dbo.T1;
+alter table dbo.T1 drop constraint pk_T1;
+
+alter table dbo.T1
+	add col2 as dbo.fn_add1(keycol) 
+	--persisted not null
+	constraint pk_T1 primary key;
