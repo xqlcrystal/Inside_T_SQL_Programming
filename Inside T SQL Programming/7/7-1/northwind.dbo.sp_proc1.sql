39ef1d0a503c4e95bce9587efbecdb4a691c65c3
xqlcrystal <xql252660@163.com>
2016-05-17 15:42:06 +0800

    存储过程

---
+++ Inside T SQL Programming/7/7-1/northwind.dbo.sp_proc1.sql
@@ -0,0 +1,32 @@
+﻿use master;
+exec sp_MS_marksystemobject 'dbo.sp_proc1'
+
+use NorthWind;
+
+exec dbo.sp_proc1;
+
+use AdventureWorks2012;
+
+exec dbo.sp_proc1;
+
+exec AdventureWorks2012.dbo.sp_proc1;
+
+
+use NorthWind;
+go
+
+if OBJECT_ID('dbo.sp_proc1') is not null
+	drop proc dbo.sp_proc1;
+go
+
+create proc dbo.sp_proc1
+as
+print 'northwind.dbo.sp_proc1 executing in '+db_name();
+go
+
+
+exec NorthWind.dbo.sp_proc1;
+
+use AdventureWorks2012;
+
+exec dbo.sp_proc1;
