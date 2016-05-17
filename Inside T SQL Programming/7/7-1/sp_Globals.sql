b3af72c021640d6291070b83dc2dbdd1a256f121
xqlcrystal <xql252660@163.com>
2016-05-17 14:58:03 +0800

    存储过程

---
+++ Inside T SQL Programming/7/7-1/sp_Globals.sql
@@ -0,0 +1,18 @@
+﻿--不要在正式环境中依赖一下行为
+
+use master;
+go
+
+create table dbo.sp_Globals
+(
+var_name sysname not null primary key,
+val sql_variant null
+);
+
+
+use NorthWind;
+insert into dbo.sp_Globals(var_name,val) values('var1',10);
+
+use Test1;
+
+insert into dbo.sp_Globals(var_name,val) values('var2',CAST(1 as bit));
