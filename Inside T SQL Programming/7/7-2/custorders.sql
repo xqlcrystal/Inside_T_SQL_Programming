0d09b3b166a239ff06ab12687bc434679861b324
xqlcrystal <xql252660@163.com>
2016-05-17 17:39:25 +0800

    存储过程

---
+++ Inside T SQL Programming/7/7-2/custorders.sql
@@ -0,0 +1,50 @@
+﻿alter proc dbo.GetCustOrders
+	@custid as nvarchar(5),
+	@fromdate as datetime ='19000101',
+	@todate as datetime ='99991231',
+	@numrows as int output
+as
+
+set nocount on;
+
+declare @err as int;
+select OrderID,CustomerID,EmployeeID,OrderDate
+from dbo.Orders
+where CustomerID=@custid and
+OrderDate>=@fromdate
+and OrderDate<=@todate;
+
+select @numrows=@@ROWCOUNT,@err=@@ERROR;
+return @err;
+
+go
+
+
+declare @myerr as int,@mynumrows as int;
+
+exec @myerr=dbo.GetCustOrders 
+@custid=N'ALFKI',@fromdate='19970101',@todate='19980101',
+@numrows=@mynumrows output;
+
+select @myerr,@mynumrows;
+
+if OBJECT_ID('tempdb..#CustOrders') is not null
+	drop table #custorders;
+go
+
+create table #custorders
+(
+orderid int not null primary key,
+customerid nchar(5) not null,
+employeeid int not null,
+orderdata datetime not null
+);
+
+declare @myerr as int,@mynumrows as int;
+insert into #custorders(orderid,customerid,employeeid,orderdata)
+exec @myerr=dbo.GetCustOrders 
+@custid=N'ALFKI',@fromdate='19970101',@todate='19980101',
+@numrows=@mynumrows output;
+
+
+select * from #custorders;
