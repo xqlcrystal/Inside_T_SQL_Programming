185c85431c063c959c0866d544f4b61483910d0d
xqlcrystal <xql252660@163.com>
2016-05-17 14:45:00 +0800

    存储过程

---
+++ Inside T SQL Programming/6/6-2/Script1.sql
@@ -0,0 +1,62 @@
+﻿set nocount on;
+use tempdb;
+go
+
+if OBJECT_ID('dbo.Employees') is not null
+	drop table dbo.Employees;
+go
+
+create table dbo.Employees
+(
+	empid int not null primary key,
+	mgrid int null references dbo.Employees,
+	empname varchar(25) not null,
+	salary money not null,
+);
+
+go
+
+
+insert into dbo.Employees(empid,mgrid,empname,salary) values(1,null,'David',$10000);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(2,1,'Eitan',$7000);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(3,1,'Ina',$7500);
+
+insert into dbo.Employees(empid,mgrid,empname,salary) values(4,2,'Seraph',$5000);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(5,2,'Jiru',$5500);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(6,2,'Steve',$4500);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(7,3,'Aaron',$5000);
+
+
+insert into dbo.Employees(empid,mgrid,empname,salary) values(8,5,'Lilach',$3500);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(9,7,'Rita',$3000);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(10,5,'Sean',$3000);
+
+insert into dbo.Employees(empid,mgrid,empname,salary) values(11,7,'Gabriel',$3000);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(12,9,'Emilia',$2000);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(13,9,'Michael',$2000);
+insert into dbo.Employees(empid,mgrid,empname,salary) values(14,9,'Didi',$1500);
+
+
+if OBJECT_ID('dbo.fn_subordinates') is not null
+	drop function dbo.fn_subordinates;
+go
+
+create function dbo.fn_subordinates(@mgrid as int) returns @subs table
+(
+empid int not null primary key clustered,
+mgrid int null,
+empname varchar(25) not null,
+salary money not null,
+lvl int not null,
+unique clustered(lvl,empid)
+)
+as
+begin
+	declare @lvl as int;
+	set @lvl=0;
+	insert into @subs(empid,mgrid,empname,salary,lvl)
+	select empid,null,empname,salary,@lvl
+	from dbo.Employees
+	where empid=@mgrid
+
+end
---
+++ Inside T SQL Programming/7/7-1/sp_proc1.sql
@@ -0,0 +1,24 @@
+﻿set nocount on;
+use master;
+go
+
+if OBJECT_ID('dbo.sp_proc1') is not null
+	drop proc dbo.sp_proc1;
+go
+
+create proc dbo.sp_proc1
+as 
+
+PRINT 'master.dbo.sp_proc1 executing in '+DB_Name();
+Exec('select table_catalog,table_schema,table_name
+	from information_schema.tables
+	where table_type=''base table'';');
+
+select table_catalog,table_schema,table_name
+from INFORMATION_SCHEMA.Tables
+where table_type='base table'
+
+go
+
+
+exec dbo.sp_proc1;
