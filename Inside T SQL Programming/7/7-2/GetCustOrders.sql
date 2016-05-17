a46e041b27214ad4aeb60f78b42ba1e38688b728
xqlcrystal <xql252660@163.com>
2016-05-17 16:39:51 +0800

    存储过程

---
+++ Inside T SQL Programming/7/7-2/GetCustOrders.sql
@@ -0,0 +1,25 @@
+﻿use NorthWind;
+go
+
+if OBJECT_ID('dbo.GetCustOrders') is not null
+	drop proc dbo.GetCustOrders;
+go
+
+create proc dbo.GetCustOrders
+	@custid as nvarchar(5),
+	@fromdate as datetime ='19000101',
+	@todate as datetime ='99991231'
+as
+
+set nocount on;
+
+select OrderID,CustomerID,EmployeeID,OrderDate
+from dbo.Orders
+where CustomerID=@custid and
+OrderDate>=@fromdate
+and OrderDate<=@todate
+
+exec dbo.GetCustOrders N'ALFKI',default,'19980101'
+
+exec dbo.GetCustOrders 
+@custid=N'ALFKI',@fromdate='19970101',@todate='19980101'
