085520c8240711fca82d299a5ba3fc2bf331362d
xqlcrystal <xql252660@163.com>
2016-05-17 15:50:58 +0800

    存储过程

---
+++ Inside T SQL Programming/7/7-1/sp_proc2.sql
@@ -0,0 +1,21 @@
+﻿use master;
+go
+
+if OBJECT_ID('dbo.sp_proc2') is not null
+	drop proc dbo.sp_proc2;
+go
+
+create proc dbo.sp_proc2
+as
+print 'master.dbo.sp_proc2 executing in '+db_name();
+
+select orderid from northwind.dbo.orders;
+go
+
+exec sp_MS_marksystemobject 'dbo.sp_proc2';
+
+use NorthWind;
+exec dbo.sp_proc2;
+
+use AdventureWorks2012;
+exec dbo.sp_proc2;
