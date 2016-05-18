34d9d2df0a5c42916b39f5c26b96cd2986a21770
xqlcrystal <xql252660@163.com>
2016-05-18 08:35:32 +0800

    重编译

---
+++ Inside T SQL Programming/7.4编译、重新编译和重用执行计划/重用执行计划/usp_GetOrders.sql
@@ -0,0 +1,44 @@
+﻿use NorthWind;
+go
+
+if OBJECT_ID('dbo.usp_GetOrders') is not null
+	drop proc dbo.usp_GetOrders;
+
+go
+
+create proc dbo.usp_GetOrders
+	@oDate as datetime
+as
+
+select OrderID,CustomerID,EmployeeID,OrderDate
+from dbo.Orders
+where OrderDate>=@oDate;
+go
+
+
+
+--打开统计选项
+set statistics io on;
+
+exec dbo.usp_GetOrders @oDate='19960101';
+
+
+select cacheobjtype,objtype,usecounts,sql
+from sys.syscacheobjects
+where sql not like '%cache%'
+	and sql like '%usp_GetOrders%'
+
+--使用重编译选项创建存储过程
+alter proc dbo.usp_GetOrders
+	@oDate as datetime
+with recompile
+as
+
+select OrderID,CustomerID,EmployeeID,OrderDate
+from dbo.Orders
+where OrderDate>=@oDate;
+go
+
+exec dbo.usp_GetOrders @oDate='19980506';
+
+exec dbo.usp_GetOrders @oDate='19960101';
