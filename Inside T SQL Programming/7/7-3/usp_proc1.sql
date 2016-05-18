ad8f648a40c72767b84434b09f74ae3fafc3f48a
xqlcrystal <xql252660@163.com>
2016-05-18 08:07:12 +0800

    存储过程机械

---
+++ Inside T SQL Programming/7/7-3/usp_proc1.sql
@@ -0,0 +1,33 @@
+﻿use Test1;
+go
+
+create proc dbo.usp_proc1
+as
+
+select col1 from dbo.T1;
+
+go
+
+
+exec dbo.usp_proc1;
+
+
+create table dbo.T1
+(
+col1 int
+);
+
+insert into dbo.T1(col1) values (1);
+
+
+exec dbo.usp_proc1;
+
+--验证已存在表T1但不存在列col2的存储过程创建
+create proc dbo.usp_proc2
+as
+select col2 from dbo.T1;
+go
+
+--执行结果
+--Msg 207, Level 16, State 1, Procedure usp_proc2, Line 28
+--列名 'col2' 无效。
