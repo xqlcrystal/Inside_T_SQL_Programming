e5e1f9cb71805a78927c9ff23c9814a71415901c
xqlcrystal <xql252660@163.com>
2016-05-19 10:16:17 +0800

    10错误处理

---
+++ Inside T SQL Programming/10错误处理/10.1Sql Server 2005之前的错误处理/Script1.sql
@@ -0,0 +1,14 @@
+﻿select * FROM Northwind.dbo.Categories;
+
+DECLARE @err AS INT;
+DECLARE @rc AS INT;
+
+--采用如下方式是不会得到正确的结果的
+--SET @err=@@ERROR;
+--实际上返回的是上一条语句SET @err=@@ERROR影响的行数，即一行
+--SET @rc=@@ROWCOUNT;
+
+SELECT @err=@@ERROR,@rc=@@ROWCOUNT;
+
+
+SELECT @err,@rc;
---
+++ Inside T SQL Programming/10错误处理/10.1Sql Server 2005之前的错误处理/usp_GetCustomerOrders.sql
@@ -0,0 +1,58 @@
+﻿USE Northwind;
+GO
+
+IF OBJECT_ID('dbo.usp_GetCustomerOrders') IS NOT NULL
+	DROP PROC dbo.usp_GetCustomerOrders;
+go
+
+CREATE PROCEDURE dbo.usp_GetCustomerOrders
+	@custid AS VARCHAR(5),
+	@fromdate AS DATETIME='19000101',
+	@todate AS DATETIME='99991231',
+	@numrows AS INT OUTPUT
+AS
+
+DECLARE @err AS INT;
+SELECT OrderID,EmployeeID,CustomerID,OrderDate
+FROM dbo.Orders
+WHERE CustomerID=@custid
+	AND OrderDate>=@fromdate
+	AND OrderDate<@todate;
+
+SELECT @numrows=@@ROWCOUNT,@err=@@ERROR;
+
+go
+
+
+
+
+
+--测试上面的程序
+SET LOCK_TIMEOUT 5000;
+DECLARE @err AS INT,@rc AS INT;
+
+--返回成功
+--EXEC @err=dbo.usp_GetCustomerOrders @custid=N'ALFKI',@fromdate='19970101',@todate='19980101',@numrows=@rc output;
+--不返回任何行
+EXEC @err=dbo.usp_GetCustomerOrders @custid=N'ALFKI',@fromdate='19980101',@todate='19980101',@numrows=@rc output;
+
+select @err AS error_number,@rc AS row_count;
+
+IF @err=0 AND @rc>0 
+BEGIN
+	PRINT 'Processing Successful';
+	RETURN;
+END
+
+IF @err=0 AND @rc=0
+BEGIN
+	PRINT 'No rows were selected';
+	RETURN;
+END
+
+IF @err=1222
+BEGIN 
+	PRINT 'handing lock timeout expired error';
+	RETURN;
+END
+	
