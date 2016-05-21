f3fc27f0141a7422efca7e5941fce944ef266340
xqlcrystal <xql252660@163.com>
2016-05-21 11:36:40 +0800

    sp_executesql更胜一筹，生成prepared的执行计划

---
+++ Inside T SQL Programming/4/4.2sp_executesql/sp_executesql.sql
@@ -0,0 +1,13 @@
+﻿--sp_executesql更胜一筹，可以充分利用执行计划，即生产Prepared的查询
+DECLARE @i AS INT;
+SET @i=10248;
+
+DECLARE @sql AS NVARCHAR(46);
+SET @sql=N'select * from dbo.orders where orderid=@id';
+
+EXEC sys.sp_executesql @stmt=@sql,
+@params=N'@id as int',
+@id=@i;
+
+SELECT cacheobjtype,objtype,usecounts,sql FROM sys.syscacheobjects
+WHERE sql LIKE '%dbo.orders%';
