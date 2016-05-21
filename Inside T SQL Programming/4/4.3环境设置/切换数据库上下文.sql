72a85845d4d31d10a4c4156231817dfee72ab5b9
xqlcrystal <xql252660@163.com>
2016-05-21 12:11:26 +0800

    使用动态sql

--- Inside T SQL Programming/4/4.3环境设置/切换数据库上下文.sql
+++ Inside T SQL Programming/4/4.3环境设置/切换数据库上下文.sql
@@ -6,3 +6,11 @@ DECLARE @db AS NVARCHAR(258);
 SET @db=QUOTENAME(N'pubs');
 EXEC(N'use '+@db+';');
 SELECT DB_NAME();
+
+USE Northwind;
+GO
+
+DECLARE @db AS NVARCHAR(258);
+SET @db=QUOTENAME(N'pubs');
+EXEC(N'use '+@db+';'+N'SELECT DB_NAME();');
+SELECT DB_NAME();
