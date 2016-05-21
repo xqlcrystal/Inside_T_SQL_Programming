82f4d692669f18cd1e372f01ab3dd8135453d7cc
xqlcrystal <xql252660@163.com>
2016-05-21 12:05:48 +0800

    环境设置

---
+++ Inside T SQL Programming/4/4.2sp_executesql/Script1.sql
@@ -0,0 +1,10 @@
+﻿DECLARE @sql AS NVARCHAR(4000),@b AS VARBINARY(1000),@s AS VARCHAR(2002);
+SET @s='0x0123456789abcdef';
+SET @sql=N'set @O='+@s+N';';
+EXEC sys.sp_executesql
+	@stmt=@sql,
+	@params=N'@O as VARBINARY(1000) output',
+	@O=@b OUTPUT;
+
+SELECT @b;
+
---
+++ Inside T SQL Programming/4/4.3环境设置/切换数据库上下文.sql
@@ -0,0 +1,8 @@
+﻿--切换数据库上下文
+USE Northwind;
+GO
+
+DECLARE @db AS NVARCHAR(258);
+SET @db=QUOTENAME(N'pubs');
+EXEC(N'use '+@db+';');
+SELECT DB_NAME();
